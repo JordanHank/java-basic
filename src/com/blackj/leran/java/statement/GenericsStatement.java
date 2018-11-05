f2441f7ae9e7f4e8a0f8af7660af7348660b7538
zhanghx <jordan.hank@hotmail.com>
2018-11-05 23:14:12 +0800

    java 泛型使用介绍

---
+++ src/com/blackj/leran/java/statement/GenericsStatement.java
@@ -0,0 +1,21 @@
+package com.blackj.leran.java.statement;
+
+/**
+ * Program Name: java-basic
+ * <p>
+ * Description: java 泛型声明使用
+ * <p>
+ * Created by Zhang.Haixin on 2018/11/5
+ *
+ * @author Zhang.Haixin
+ * @version 1.0
+ */
+public class GenericsStatement {
+
+    /**
+     *
+     */
+    public static void main(String [] args) {
+
+    }
+}
